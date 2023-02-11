807697d120f6ea1cc7a92c20a03e0cf7d3f21fe6
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-02-11 10:28:44 -0800

    get api

--- src/main/java/com/ryanmello/Springboot/tutorial/controller/DepartmentController.java
+++ src/main/java/com/ryanmello/Springboot/tutorial/controller/DepartmentController.java
@@ -2,11 +2,13 @@ package com.ryanmello.Springboot.tutorial.controller;
 
 import com.ryanmello.Springboot.tutorial.entity.Department;
 import com.ryanmello.Springboot.tutorial.service.DepartmentService;
-import com.ryanmello.Springboot.tutorial.service.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -17,5 +19,10 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> getDepartmentList(){
+        return departmentService.getDepartmentList();
     }
 }
--- src/main/java/com/ryanmello/Springboot/tutorial/service/DepartmentService.java
+++ src/main/java/com/ryanmello/Springboot/tutorial/service/DepartmentService.java
@@ -2,6 +2,9 @@ package com.ryanmello.Springboot.tutorial.service;
 
 import com.ryanmello.Springboot.tutorial.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+    public List<Department> getDepartmentList();
 }
